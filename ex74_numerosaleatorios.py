7d24642a46fa5b74ab556aabfc456e1efdb59274
Adam <119963962+adamribeiro22@users.noreply.github.com>
2025-03-11 16:13:19 -0300

    Add files via upload

---
+++ ex74_numerosaleatorios.py
@@ -0,0 +1,5 @@
+from random import randint
+numeros = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10), )
+print(f'Os números são: {numeros}')
+print(f'O maior é: {max(numeros)}')
+print(f'O menor é: {min(numeros)}')
